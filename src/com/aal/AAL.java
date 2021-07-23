0452ec88a2487bc01edd4d8047d0a552fbb203b6
Orress <orress35@mail.ru>
2021-07-23 20:30:58 +0300

    Added plugin.yml

---
+++ src/com/aal/AAL.java
@@ -0,0 +1,16 @@
+package com.aal;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AAL extends JavaPlugin {
+    private static AAL instance;
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() { }
+
+}
--- src/com/aal/AdvancedAntiLegit.java
+++
@@ -1,6 +0,0 @@
-package com.aal;
-
-import org.bukkit.plugin.java.JavaPlugin;
-
-public final class AdvancedAntiLegit extends JavaPlugin {
-}
